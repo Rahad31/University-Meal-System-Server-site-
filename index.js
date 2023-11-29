96900af125ac2e65f4bcae3e794f8a1de0abacda
Rahad31 <rahad3100@gmail.com>
2023-11-29 19:17:02 +0600

    user mangement read write

--- index.js
+++ index.js
@@ -25,14 +25,65 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-
+    const mealCollection = client.db("unimealDB").collection("meal");
+     const mealupCollection = client.db("unimealDB").collection("mealup");
+    const usersCollection = client.db("unimealDB").collection("users");
     
+     const reqmealCollection = client.db("unimealDB").collection("reqmeal");
+    app.post("/meal", async (req, res) => {
+      const mealdetail = req.body;
+      console.log(mealdetail);
+      const result = await mealCollection.insertOne(mealdetail);
+      res.send(result);
+    });
+
+       app.post("/mealup", async (req, res) => {
+         const mealdetail = req.body;
+         console.log(mealdetail);
+         const result = await mealupCollection.insertOne(mealdetail);
+         res.send(result);
+       });
+
+           app.post("/users", async (req, res) => {
+             const user = req.body;
+             // insert email if user doesnt exists:
+             // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+             const query = { email: user.email };
+             const existingUser = await usersCollection.findOne(query);
+             if (existingUser) {
+               return res.send({
+                 message: "user already exists",
+                 insertedId: null,
+               });
+             }
+             const result = await usersCollection.insertOne(user);
+             res.send(result);
+           });
+            app.get("/users", async (req, res) => {
+              const cursor = usersCollection.find();
+              const result = await cursor.toArray();
+              res.send(result);
+            });
+
+         app.get("/meal", async (req, res) => {
+           const cursor = mealCollection.find();
+           const result = await cursor.toArray();
+           res.send(result);
+         });
+
+         app.post("/reqmeal", async (req, res) => {
+           const mealdetail = req.body;
+           console.log(mealdetail);
+           const result = await reqmealCollection.insertOne(mealdetail);
+           res.send(result);
+         });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+     await client.close();
   }
 }
 run().catch(console.dir);
